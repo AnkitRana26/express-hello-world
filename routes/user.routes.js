a66a6ece9e476d1c2a9dd0d7cf728d2b7be611ba
Ankit Rana <109352490+AnkitRana26@users.noreply.github.com>
2023-01-04 16:26:56 +0530

    Server Deployed:

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,80 +1,98 @@
-const express=require("express");
+const express = require("express");
 const User = require("../database/user");
-let jwt=require("jsonwebtoken")
-let argon2=require("argon2");
+let jwt = require("jsonwebtoken")
+let argon2 = require("argon2");
 const { checkToken } = require("../miscFunction/CommonFunction");
-const app=express.Router();
-app.post("/signup",async(req,res)=>{
-    let {email,password,firstName,lastName,contact}=req.body;
+const app = express.Router();
+app.post("/signup", async (req, res) => {
+    let { email, password, firstName, lastName, contact } = req.body;
     try {
-        password=await argon2.hash(password)
-        
-       let data=await User.create({email,password,firstName,contact,lastName})
-       return res.status(201).send(data)
+        password = await argon2.hash(password)
+
+        let data = await User.create({ email, password, firstName, contact, lastName })
+        return res.status(201).send({
+            data: data
+        })
     } catch (errr) {
-        res.status(400).send(errr.message)
+        res.status(400).send({
+            err: errr.message
+        })
     }
 })
-app.post("/login",async(req,res)=>{
-    let {email,password}=req.body
-    console.log(email)
-try{
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body
+    // console.log(email)
+    try {
 
-    let user=await User.findOne({email})
+        let user = await User.findOne({ email })
 
-    if(user){
-      let varifieduser=  await argon2.verify(user.password, password)
-    if(varifieduser){
-        let token=jwt.sign({id:user._id,email:user.email},process.env.secret)
-        
-      
-     // localStorage.setItem("token",token)
-       return  res.status(200).send(token)
-    
-        
-    }else{
-        return res.status(404).send("Invalid credentials")
+        if (user) {
+            let varifieduser = await argon2.verify(user.password, password)
+            if (varifieduser) {
+                let token = jwt.sign({ id: user._id, email: user.email }, process.env.secret)
+
+
+                // localStorage.setItem("token",token)
+                return res.status(200).send({
+                    data: token
+                })
+
+
+            } else {
+                return res.status(404).send({
+                    err: "Invalid Credentials"
+                })
+            }
+
+        } else {
+            return res.status(404).send({
+                err: "User Not Found"
+            })
+        }
+
+    } catch (e) {
+
+        res.status(401).send({
+            err: e.message
+        })
     }
-    
-    }else{
-        return res.status(404).send("user not found")
-    }
-
-}catch(e){
-   
-    res.status(401).send(e.message)
-}
 })
 
-app.get('/loggedIn/:token',async(req,res)=>{
+app.get('/loggedIn/:token', async (req, res) => {
     let token = req.params.token;
-    try{
-        let data= await checkToken(token);
+    try {
+        let data = await checkToken(token);
         res.send({
-            data:data
+            data: data
         })
-    }catch(err){
+    } catch (err) {
         res.send({
-            err:err.message
+            err: err.message
         })
     }
 })
-app.post("/admin/login",(req,res)=>{
-let {email,password}=req.body
-try {
-    if(email==process.env.Admin_email && password==process.env.Admin_password){
-        let token=jwt.sign({"role":"admin"},process.env.Admin_secret)
-  return   res.status(200).send(token)
-} else{
-return res.status(400).send("wrong credential")
-}
-}catch (error) {
-   return  res.status(400).send("wrong credentials") 
-}
+app.post("/admin/login", (req, res) => {
+    let { email, password } = req.body
+    try {
+        if (email == process.env.Admin_email && password == process.env.Admin_password) {
+            let token = jwt.sign({ "role": "admin" }, process.env.Admin_secret)
+            return res.status(200).send({
+                data: token
+            })
+        } else {
+            return res.status(400).send({
+                err: "wrong credential"
+            })
+        }
+    } catch (error) {
+        return res.status(400).send({
+            err: "wrong credentials"
+        })
+    }
 
 
 })
 
 
 
-module.exports=app
+module.exports = app
